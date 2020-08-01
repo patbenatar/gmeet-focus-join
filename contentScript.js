8f092d3fb42a45105f1df60e495537f2a8ccae9e
Nick Giancola <patbenatar@gmail.com>
2020-08-01 10:12:58 -0700

    Whitespace

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,6 @@
 const urlPattern = /https:\/\/meet\.google\.com\/(_meet\/)?[a-z]{3}-[a-z]{4}-[a-z]{3}/
 
 if (window.location.href.match(urlPattern)) {
-
   const timer = setInterval(() => {
     const loadingEl = document.evaluate("//div[text()='Getting ready...']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
     const joinButton = document.evaluate("//span[text()='Join now']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
