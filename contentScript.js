515f2817c398ff5aa431c3b7ef69323b3da1a4be
Nick Giancola <patbenatar@gmail.com>
2020-08-01 10:12:45 -0700

    Auto-focus 'Join now' button

---
+++ contentScript.js
@@ -0,0 +1,15 @@
+const urlPattern = /https:\/\/meet\.google\.com\/(_meet\/)?[a-z]{3}-[a-z]{4}-[a-z]{3}/
+
+if (window.location.href.match(urlPattern)) {
+
+  const timer = setInterval(() => {
+    const loadingEl = document.evaluate("//div[text()='Getting ready...']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+    const joinButton = document.evaluate("//span[text()='Join now']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+
+    if (loadingEl) {
+    } else if (joinButton) {
+      joinButton.parentNode.parentNode.focus()
+      clearInterval(timer)
+    }
+  }, 250)
+}
